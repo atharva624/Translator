aeb9cd24fd96895383dc8494130d2dc4d066a01e
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-08 12:57:32 +0530

    Update Triangulation.h

--- Translator/Translator/Header/Triangulation.h
+++ Translator/Translator/Header/Triangulation.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "Point.h"
+#include "Triangle.h"
+#include <vector>
+
+class Triangulation {
+private:
+    std::vector<double> points;
+
+public:
+    std::vector<Triangle> addUniquePoints(std::vector<Point> points);
+};
