22e7f9967dd95b2262e6a185126a207d49e2f71f
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-08 13:00:23 +0530

    Update Triangulation.cpp

--- Translator/Translator/Triangulation.cpp
+++ Translator/Translator/Triangulation.cpp
@@ -1,19 +1,15 @@
 #include "Triangulation.h"
-#include "Point.h"
-#include "Reader.h"
-#include "Triangle.h"
-#include <vector>
 
-vector<Triangle> Triangulation::addUniquePoints(vector<Point> points) {
-    vector<Triangle> uniqueTriangles;
+std::vector<Triangle> Triangulation::addUniquePoints(std::vector<Point> points) {
+    std::vector<Triangle> uniqueTriangles;
 
     if (points.size() % 3 == 0) {
         int triangleCount = points.size() / 3;
 
         for (int i = 0; i < triangleCount; i++) {
-            Point vertex1(points[(i * 3) + 0].x, points[(i * 3) + 0].y, points[(i * 3) + 0].z);
-            Point vertex2(points[(i * 3) + 1].x, points[(i * 3) + 1].y, points[(i * 3) + 1].z);
-            Point vertex3(points[(i * 3) + 2].x, points[(i * 3) + 2].y, points[(i * 3) + 2].z);
+            Point vertex1(points[(i * 3) + 0].getX(), points[(i * 3) + 0].getY(), points[(i * 3) + 0].getZ());
+            Point vertex2(points[(i * 3) + 1].getX(), points[(i * 3) + 1].getY(), points[(i * 3) + 1].getZ());
+            Point vertex3(points[(i * 3) + 2].getX(), points[(i * 3) + 2].getY(), points[(i * 3) + 2].getZ());
             Triangle triangle(vertex1, vertex2, vertex3);
             uniqueTriangles.push_back(triangle);
         }
