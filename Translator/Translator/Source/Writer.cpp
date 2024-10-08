53da7791dcf8606a26a55905a775915ec8c9eb93
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-08 12:59:35 +0530

    Update Writer.cpp

--- Translator/Translator/Source/Writer.cpp
+++ Translator/Translator/Source/Writer.cpp
@@ -1,15 +1,15 @@
 #include "Writer.h"
 
-void Writer::writer(vector<Triangle> triangles, vector<double> points) {
-    ofstream outputFile("output.dat");
+void Writer::writer(std::vector<Triangle> triangles, std::vector<double> points) {
+    std::ofstream outputFile("output.dat");
 
     for (const auto& triangle : triangles) {
-        outputFile << points[triangle.p1.x] << " " << points[triangle.p1.y] << " " << points[triangle.p1.z] << endl;
-        outputFile << points[triangle.p2.x] << " " << points[triangle.p2.y] << " " << points[triangle.p2.z] << endl;
-        outputFile << points[triangle.p3.x] << " " << points[triangle.p3.y] << " " << points[triangle.p3.z] << endl;
-        outputFile << points[triangle.p1.x] << " " << points[triangle.p1.y] << " " << points[triangle.p1.z] << endl;
-        outputFile << endl;
-        outputFile << endl;
+        outputFile << points[triangle.getP1().getX()] << " " << points[triangle.getP1().getY()] << " " << points[triangle.getP1().getZ()] << std::endl;
+        outputFile << points[triangle.getP2().getX()] << " " << points[triangle.getP2().getY()] << " " << points[triangle.getP2().getZ()] << std::endl;
+        outputFile << points[triangle.getP3().getX()] << " " << points[triangle.getP3().getY()] << " " << points[triangle.getP3().getZ()] << std::endl;
+        outputFile << points[triangle.getP1().getX()] << " " << points[triangle.getP1().getY()] << " " << points[triangle.getP1().getZ()] << std::endl;
+        outputFile << std::endl;
+        outputFile << std::endl;
     }
 
     outputFile.close();
