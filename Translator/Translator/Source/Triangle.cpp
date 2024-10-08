1b569c8d3699f17b0408e3f628ef31d7ea4546bd
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-08 12:59:12 +0530

    Update Triangle.cpp

--- Translator/Translator/Source/Triangle.cpp
+++ Translator/Translator/Source/Triangle.cpp
@@ -1,6 +1,10 @@
 #include "Triangle.h"
-#include "Point.h"
 
 Triangle::Triangle(Point vertex1, Point vertex2, Point vertex3) : p1(vertex1), p2(vertex2), p3(vertex3) {}
 
 Triangle::~Triangle() {}
+
+const Point& Triangle::getP1() const { return p1; }
+const Point& Triangle::getP2() const { return p2; }
+const Point& Triangle::getP3() const { return p3; }
+
