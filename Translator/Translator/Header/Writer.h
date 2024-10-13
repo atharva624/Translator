f961a25348b64f9a8c354bea69a7528785486495
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-13 13:20:54 +0530

    Update Writer.h

--- Translator/Translator/Header/Writer.h
+++ Translator/Translator/Header/Writer.h
@@ -5,7 +5,8 @@
 #include "Point.h"
 #include "Triangle.h"
 
+// Class for writing triangle and point data to a file
 class Writer {
 public:
-    void writer(std::vector<Triangle> getTriangles, std::vector<double> getPoints);
+    void writer(std::vector<Triangle> getTriangles, std::vector<double> getPoints); // Method to write triangles and points to a file
 };
