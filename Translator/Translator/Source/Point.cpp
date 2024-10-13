d24d1fa0c034e3a3b0d576d703ea86d9da1aed95
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-13 13:22:45 +0530

    Update Point.cpp

--- Translator/Translator/Source/Point.cpp
+++ Translator/Translator/Source/Point.cpp
@@ -1,11 +1,16 @@
 #include "Point.h"
 
+// Constructor to initialize the point's coordinates
 Point::Point(int xCoord, int yCoord, int zCoord) : x(xCoord), y(yCoord), z(zCoord) {}
 
+// Destructor
 Point::~Point() {}
 
-
-
+// Getter for the x coordinate
 int Point::getX() const { return x; }
+
+// Getter for the y coordinate
 int Point::getY() const { return y; }
+
+// Getter for the z coordinate
 int Point::getZ() const { return z; }
