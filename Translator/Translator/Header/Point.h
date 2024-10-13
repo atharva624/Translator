67f9a40686b883405b0e0118232ece9aafc8e774
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-13 13:17:38 +0530

    Update Point.h

--- Translator/Translator/Header/Point.h
+++ Translator/Translator/Header/Point.h
@@ -2,15 +2,16 @@
 #include <iostream>
 using namespace std;
 
+// Class representing a point in 3D space
 class Point {
 private:
-    int x, y, z;
+    int x, y, z; // Coordinates
 
 public:
-    Point(int x, int y, int z);
-    ~Point();
-    void getCoordinates() const;
-    int getX() const;
-    int getY() const;
-    int getZ() const;
+    Point(int x, int y, int z); // Constructor to initialize coordinates
+    ~Point(); // Destructor
+
+    int getX() const; // Getter for x coordinate
+    int getY() const; // Getter for y coordinate
+    int getZ() const; // Getter for z coordinate
 };
