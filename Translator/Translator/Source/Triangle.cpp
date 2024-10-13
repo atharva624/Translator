e6d595c66452fb3b51e38e4447fb353ae5ee7a94
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-13 13:24:30 +0530

    Update Triangle.cpp

--- Translator/Translator/Source/Triangle.cpp
+++ Translator/Translator/Source/Triangle.cpp
@@ -1,10 +1,12 @@
 #include "Triangle.h"
 
+// Constructor to initialize the triangle's vertices
 Triangle::Triangle(Point vertex1, Point vertex2, Point vertex3) : p1(vertex1), p2(vertex2), p3(vertex3) {}
 
+// Destructor
 Triangle::~Triangle() {}
 
+// Getters for the triangle's vertices
 const Point& Triangle::getP1() const { return p1; }
 const Point& Triangle::getP2() const { return p2; }
 const Point& Triangle::getP3() const { return p3; }
-
