50897d2d928e479cae44d567c20d10b699d56ef3
atharva624 <atharva.hambarde@cctech.co.in>
2024-10-13 13:31:17 +0530

    Update Translator.cpp

--- Translator/Translator/Translator.cpp
+++ Translator/Translator/Translator.cpp
@@ -6,15 +6,15 @@
 using namespace std;
 
 int main() {
-    Reader re;
-    vector<Point> get_points = re.reader();
-    vector<double> u_points = re.unique();
+    Reader re; // Create a Reader object
+    vector<Point> get_points = re.reader(); // Read points from the STL file
+    vector<double> u_points = re.unique(); // Get unique coordinates
 
-    Triangulation t;
-    vector<Triangle> get_triangles = t.addUniquePoints(get_points);
+    Triangulation t; // Create a Triangulation object
+    vector<Triangle> get_triangles = t.addUniquePoints(get_points); // Generate triangles from unique points
 
-    Writer w;
-    w.writer(get_triangles, u_points);
+    Writer w; // Create a Writer object
+    w.writer(get_triangles, u_points); // Write triangles and unique points to the output file
 
-    return 0;
+    return 0; 
 }
